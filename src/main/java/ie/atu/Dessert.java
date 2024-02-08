db913039d2226f7ab155b7b2f6de0cd84ab75297
Nasir Ahmad <G00419630@atu.ie>
2024-02-08 12:11:15 +0000

    Added Dessert class

--- src/main/java/ie/atu/Dessert.java
+++ src/main/java/ie/atu/Dessert.java
@@ -1,11 +1,11 @@
 package ie.atu;
 
-public class Burger implements MenuItem{
+public class Dessert implements MenuItem{
     private double price;
     private String name;
     private String ingredients;
 
-    public Burger(double price, String name, String ingredients) {
+    public Dessert(double price, String name, String ingredients) {
         this.price = price;
         this.name = name;
         this.ingredients = ingredients;
